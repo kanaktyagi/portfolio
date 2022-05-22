ee46eb8662f667d80ed7609ba01724fdec4bad67
kanaktyagi <tyagiknk.india@gmail.com>
2022-05-22 22:06:12 +0530

    Added the name in the Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className='n-wrapper'>
         <div className='n-left'>
-            <div className='n-name'>Andrew</div>
+            <div className='n-name'>Kanak</div>
             <span>toggle</span>
         </div>
         <div className='n-right'>
