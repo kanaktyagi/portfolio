dc9310edd1ba13a45616c83b0c907e75bfe0d71c
kanaktyagi <tyagiknk.india@gmail.com>
2022-06-02 00:06:17 +0530

    added the context for theme change

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ import Portfolio from "./Components/Portfolio/Portfolio";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import { useContext } from "react";
+import { ThemeContext } from "./Context";
 
 function App() {
+  const theme = useContext(ThemeContext)
+  const{ darkMode} = theme.state
   return (
-    <div className="App">
+    <div className="App"
+    style={{
+      background: darkMode ? 'black': '',
+      color: darkMode ? 'white': '' ,
+    }}
+    >
       <Navbar/>
       <Intro/>
       <Services/>
